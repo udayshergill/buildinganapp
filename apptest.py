d30bc39b8f1f0dbdd0eed7f7aec1c231e8fc4e05
Uday Shergill <86528848+udayshergill@users.noreply.github.com>
2021-07-09 10:56:04 -0400

    Add files via upload
    Made an app that can open a file directory and look for exe files. 
    Next steps: save files opened and run them whenever needed to.

--- apptest.py
+++ apptest.py
@@ -1,12 +1,17 @@
+import tkinter as tk #import library
+from tkinter import filedialog, Text #bringing stuff from library
 import os
-os.environ['KIVY_IMAGE'] = 'pil,sdl2'
 
-#kivy program
-from kivy.app import App
-from kivy.uix.button import Button
+root = tk.Tk() #setting up the main folder where everything attaches to
+canvas = tk.Canvas(root, height=700, width=700, bg="#34d3eb") #making the canvas, delaring the root then changing height, width, and background color
+canvas.pack() #attaching the canvas to the root
+def addApp():
+    filename= filedialog.askopenfilename(initialdir="/", title="Select File", 
+    filetypes=(("executables", "*.exe"), ("allfiles", "*.*")))
 
-class TestApp(App):
-    def build(self):
-        return Button(text='Hello World')
+frame = tk.Frame(root, bg="white")#Created a frame and attached it to the root made the bg white
+frame.place(relwidth= 0.8, relheight= 0.8, relx= 0.1, rely= 0.1) #placed frame on gui and given it a width, height, and a x and y location
 
-TestApp().run()
+openFile = tk.Button(frame, text = "Open File", padx=10, pady=7, fg="black", bg="white", command = addApp) #created a button on the frame, with text "Open File", set x, set y, set foreground color, can set bg color, set a command to open an app with button
+openFile.pack() #places the button
+root.mainloop()
